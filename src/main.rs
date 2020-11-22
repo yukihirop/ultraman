857a6b6f9c2c95e4fba20c637d87a7ec63d6305f
yukihirop <te108186@gmail.com>
2020-11-22 22:02:44 +0900

    extract procfile

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use nix::sys::signal::{self as nix_signal, Signal};
 use nix::sys::wait::WaitStatus;
 use nix::{self, unistd::Pid};
-use std::collections::HashMap;
 use std::process::{exit, Command, Stdio};
 use std::sync::{Arc, Barrier, Mutex};
 use std::thread;
@@ -11,48 +10,16 @@ mod output;
 mod process;
 mod signal;
 mod stream_read;
-
-struct Script {
-    cmd: String,
-    concurrency: usize,
-}
+mod procfile;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut proc_handles = vec![];
     let procs: Arc<Mutex<Vec<Arc<Mutex<process::Process>>>>> = Arc::new(Mutex::new(vec![]));
-
-    let mut scripts = HashMap::<&str, Script>::new();
-    scripts.insert(
-        "loop",
-        Script {
-            cmd: String::from("./bin/loop.sh"),
-            concurrency: 2,
-        },
-    );
-    scripts.insert(
-        "exit_1",
-        Script {
-            cmd: String::from("./bin/exit_1.sh"),
-            concurrency: 1,
-        },
-    );
-    scripts.insert(
-        "exit_0",
-        Script {
-            cmd: String::from("./bin/exit_0.sh"),
-            concurrency: 1,
-        },
-    );
-
-    let mut concurrencies = vec![];
-    for (_, script) in &scripts {
-        concurrencies.push(script.concurrency);
-    }
-    let process_len = concurrencies.iter().fold(0, |sum, a| sum + a);
+    let scripts = procfile::scripts();
+    let process_len = procfile::process_ln();
+    let padding = procfile::padding();
 
     let barrier = Arc::new(Barrier::new(process_len + 1));
-    // e.g) <name>.<concurrency> |
-    let padding = scripts.keys().map(|name| name.len()).max().unwrap() + 3;
     let mut index = 0;
 
     for (key, script) in scripts {
---
+++ src/procfile.rs
@@ -0,0 +1,43 @@
+use std::collections::HashMap;
+
+pub struct Script {
+  pub cmd: String,
+  pub concurrency: usize,
+}
+
+// https://www.366service.com/jp/qa/3b42bd30380c728939a2e80b42d430a6
+macro_rules! hashmap {
+  ($( $key: expr => $val: expr), *) =>{{
+    let mut map = ::std::collections::HashMap::new();
+    $( map.insert($key, $val); )*
+    map
+  }}
+}
+
+pub fn scripts()-> HashMap<&'static str, Script> {
+  let scripts: HashMap<&str, Script> = hashmap! [
+    "loop" => Script {
+      cmd: String::from("./bin/loop.sh"),
+      concurrency: 2,
+    },
+    "exit_1" => Script {
+      cmd: String::from("./bin/exit_1.sh"),
+      concurrency: 1,
+    },
+    "exit_0" => Script {
+      cmd: String::from("./bin/exit_0.sh"),
+      concurrency: 1,
+    }
+  ];
+
+  scripts
+}
+
+pub fn padding() -> usize {
+  // e.g) <name>.<concurrency> |
+  scripts().keys().map(|name| name.len()).max().unwrap() + 3
+}
+
+pub fn process_ln() -> usize {
+  scripts().values().map(|s| s.concurrency).fold(0, |sum, a| sum + a)
+}
